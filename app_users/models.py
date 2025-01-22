26f47e341f678d2c8fd774291191d7ea65a8fcaa
Akhmedov76 <ahmedovj7686@gmail.com>
2025-01-22 18:09:37 +0500

    Update models and create requirements.txt and change conf file

--- app_users/models.py
+++ app_users/models.py
@@ -1,12 +1,42 @@
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 from django.db import models
-from django.contrib.auth.models import AbstractUser
 
-class User(AbstractUser):
-    email = models.EmailField(unique=True)
-    phone_number = models.CharField(max_length=15, unique=True, db_index=True)
-    date_of_birth = models.DateField()
-    language_preference = models.CharField(max_length=20, choices=[('Lang1', 'Lang1'), ('Lang2', 'Lang2')], default='Lang1')
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, mobile_number, password=None, **extra_fields):
+        if not email and not mobile_number:
+            raise ValueError('Email yoki mobil raqam kiritilishi shart.')
+        if email:
+            email = self.normalize_email(email)
+            extra_fields.setdefault('username', email.split('@')[0])
+        user = self.model(email=email, mobile_number=mobile_number, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, mobile_number, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(email, mobile_number, password, **extra_fields)
+
+
+class CustomUser(AbstractBaseUser):
+    email = models.EmailField(unique=True, null=True, blank=True)
+    mobile_number = models.CharField(max_length=15, unique=True, null=True, blank=True)
+    first_name = models.CharField(max_length=50, blank=True)
+    last_name = models.CharField(max_length=50, blank=True)
+    date_of_birth = models.DateField(null=True, blank=True)
+    language_preference_1 = models.CharField(max_length=50, null=True, blank=True)
+    language_preference_2 = models.CharField(max_length=50, null=True, blank=True)
+    learning_goal = models.TextField(null=True, blank=True)
+
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['mobile_number']
 
     def __str__(self):
-        return self.username
-
+        return self.email if self.email else self.mobile_number
--- conf/urls.py
+++ conf/urls.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/users/', include('app_users.urls')),
 
+]
+
+urlpatterns += [
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
