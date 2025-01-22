ee583b7bf4cdbc2fb90f7aa6f889860c549c7644
Akhmedov76 <ahmedovj7686@gmail.com>
2025-01-22 17:54:09 +0500

    Create user model

--- app_users/models.py
+++ app_users/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=15, unique=True, db_index=True)
+    date_of_birth = models.DateField()
+    language_preference = models.CharField(max_length=20, choices=[('Lang1', 'Lang1'), ('Lang2', 'Lang2')], default='Lang1')
+
+    def __str__(self):
+        return self.username
+
