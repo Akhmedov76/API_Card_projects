bc174ee6045bd307ca20c571070da880684d9137
Akhmedov76 <ahmedovj7686@gmail.com>
2025-02-15 17:41:00 +0500

    add chat model

--- app_chats/models.py
+++ app_chats/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+from app_users.models import User
+
+
+class Chat(models.Model):
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sent_messages')
+    receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name='received_messages')
+    message = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    is_read = models.BooleanField(default=False)
