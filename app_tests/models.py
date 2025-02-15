eb633a7c859a4abe50e4d838bfe254db391306e0
Akhmedov76 <ahmedovj7686@gmail.com>
2025-02-15 17:38:38 +0500

    add tests model

--- app_tests/models.py
+++ app_tests/models.py
@@ -1,1 +1,8 @@
 from django.db import models
+from app_teams.models import Team
+
+
+class Test(models.Model):
+    team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='tests')
+    questions = models.TextField()
+    answers = models.TextField()
