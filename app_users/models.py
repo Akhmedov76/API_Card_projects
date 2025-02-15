5de7af8c30b5b1169262788856db36f7b67eec1f
Akhmedov76 <ahmedovj7686@gmail.com>
2025-02-15 15:58:57 +0500

    create all we use app and add user model

--- app_users/models.py
+++ app_users/models.py
@@ -1,42 +1,40 @@
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.db import models
 
 
-class CustomUserManager(BaseUserManager):
-    def create_user(self, email, mobile_number, password=None, **extra_fields):
-        if not email and not mobile_number:
-            raise ValueError('Email yoki mobil raqam kiritilishi shart.')
-        if email:
-            email = self.normalize_email(email)
-            extra_fields.setdefault('username', email.split('@')[0])
-        user = self.model(email=email, mobile_number=mobile_number, **extra_fields)
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("Sorry, you have to do it via email!!!")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, email, mobile_number, password=None, **extra_fields):
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
-        return self.create_user(email, mobile_number, password, **extra_fields)
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        return self.create_user(email, password, **extra_fields)
 
 
-class CustomUser(AbstractBaseUser):
-    email = models.EmailField(unique=True, null=True, blank=True)
-    mobile_number = models.CharField(max_length=15, unique=True, null=True, blank=True)
-    first_name = models.CharField(max_length=50, blank=True)
-    last_name = models.CharField(max_length=50, blank=True)
-    date_of_birth = models.DateField(null=True, blank=True)
-    language_preference_1 = models.CharField(max_length=50, null=True, blank=True)
-    language_preference_2 = models.CharField(max_length=50, null=True, blank=True)
-    learning_goal = models.TextField(null=True, blank=True)
-
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    birth_date = models.DateField(null=True, blank=True)
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+    role = models.CharField(max_length=50, choices=[('user', 'User'), ('admin', 'Admin')])
+    preferred_language = models.CharField(max_length=50, blank=True, null=True)
+    bio = models.TextField(blank=True, null=True)
+    level = models.IntegerField(default=0)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
-    objects = CustomUserManager()
+    objects = UserManager()
 
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['mobile_number']
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["first_name", "last_name"]
 
     def __str__(self):
-        return self.email if self.email else self.mobile_number
+        return self.email
