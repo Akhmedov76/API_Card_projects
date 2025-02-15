1b9ff69cf426eb41b0e614b2e34be8cfcae566f7
Akhmedov76 <ahmedovj7686@gmail.com>
2025-02-15 17:21:58 +0500

    add wodrs model

--- app_words/models.py
+++ app_words/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+from app_teams.models import Team
+
+
+class WordCard(models.Model):
+    word = models.CharField(max_length=100)
+    translation = models.CharField(max_length=100)
+    example = models.TextField()
+    team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='word_cards')
+    created_at = models.DateTimeField(auto_now_add=True)
