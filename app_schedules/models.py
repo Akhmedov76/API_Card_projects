90c92810ef1c288ccf3c914a76fa2ff779bcf514
Akhmedov76 <ahmedovj7686@gmail.com>
2025-02-15 17:43:41 +0500

    add schedul and appointment model

--- app_schedules/models.py
+++ app_schedules/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+from app_users.models import User
+from app_words.models import WordCard
+
+
+class Schedule(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='schedules')
+    word_card = models.ForeignKey(WordCard, on_delete=models.CASCADE)
+    date = models.DateField()
+    time = models.TimeField()
+    status = models.CharField(max_length=50)
+
+
+class Appointment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='appointments')
+    appointment_date = models.DateField()
+    appointment_time = models.TimeField()
+    purpose = models.TextField()
+    location = models.CharField(max_length=255)
+    status = models.CharField(max_length=50)
+    feedback = models.TextField(blank=True, null=True)
+    confirmation_status = models.BooleanField(default=False)
