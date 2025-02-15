ae3ed94f43d95849aaf8879bada0f7f10feb6768
Akhmedov76 <ahmedovj7686@gmail.com>
2025-02-15 17:39:48 +0500

    add game model

--- app_games/models.py
+++ app_games/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+from app_teams.models import Team
+
+
+class MiniGame(models.Model):
+    game_type = models.CharField(max_length=100)
+    team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='mini_games')
+    results = models.TextField()
