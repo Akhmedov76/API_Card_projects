a085508a7fe8061335b02830055f1070fe524da6
Akhmedov76 <ahmedovj7686@gmail.com>
2025-02-15 17:41:51 +0500

    add notification model

--- app_notifications/models.py
+++ app_notifications/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+from app_users.models import User
+
+
+class Notification(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='notifications')
+    title = models.CharField(max_length=255)
+    message = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    is_read = models.BooleanField(default=False)
