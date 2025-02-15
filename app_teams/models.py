9227bec447e0fb6e99e5f6376d68d6069828966d
Akhmedov76 <ahmedovj7686@gmail.com>
2025-02-15 17:19:35 +0500

    add team model

--- app_teams/models.py
+++ app_teams/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+from app_users.models import User
+
+
+class Team(models.Model):
+    name = models.CharField(max_length=100)
+    members = models.ManyToManyField(User, related_name='teams')
+    created_at = models.DateTimeField(auto_now_add=True)
