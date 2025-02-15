dbb477ea53b87bd5a6a72f18ef379a515cd7892b
Akhmedov76 <ahmedovj7686@gmail.com>
2025-02-15 17:35:51 +0500

    add privete model in words

--- app_words/models.py
+++ app_words/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 from app_teams.models import Team
+from app_users.models import User
 
 
 class WordCard(models.Model):
@@ -9,3 +10,11 @@ class WordCard(models.Model):
     example = models.TextField()
     team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='word_cards')
     created_at = models.DateTimeField(auto_now_add=True)
+
+
+class PrivateCard(models.Model):
+    word = models.CharField(max_length=100)
+    translation = models.CharField(max_length=100)
+    description = models.TextField()
+    status = models.BooleanField(default=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='private_cards')
