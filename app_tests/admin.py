5e3f5a8f1ef38a8482a0841781cc305d9b55632c
Akhmedov76 <ahmedovj7686@gmail.com>
2025-02-16 23:38:04 +0500

    create admin register

--- app_tests/admin.py
+++ app_tests/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from app_tests.models import Test
+
+
+class TestAdmin(admin.ModelAdmin):
+    list_display = ('id', 'team', 'questions', 'answers')
+    list_filter = ('team',)
+    search_fields = ('questions', 'answers')
+    ordering = ('-id',)
+    fields = ('team', 'questions', 'answers')
+
+admin.site.register(Test, TestAdmin)
