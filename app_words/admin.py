ba7c5ba141977c5f9acca6296b460ad64c9c70e9
Akhmedov76 <ahmedovj7686@gmail.com>
2025-02-16 23:37:38 +0500

    create admin register

--- app_words/admin.py
+++ app_words/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from app_words.models import WordCard, PrivateCard
+
+
+class WordCardAdmin(admin.ModelAdmin):
+    list_display = ('id', 'word', 'translation', 'example', 'team')
+    list_filter = ('team',)
+    search_fields = ('word', 'translation', 'example')
+    ordering = ('-id',)
+    fields = ('word', 'translation', 'example', 'team')
+
+
+class PrivateCardAdmin(admin.ModelAdmin):
+    list_display = ('id', 'word', 'translation', 'description', 'user')
+    list_filter = ('user',)
+    search_fields = ('word', 'translation', 'description')
+    ordering = ('-id',)
+    fields = ('word', 'translation', 'description', 'user')
+
+
+admin.site.register(WordCard, WordCardAdmin)
+admin.site.register(PrivateCard, PrivateCardAdmin)
