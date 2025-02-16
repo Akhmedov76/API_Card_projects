354572095711b3775056e0b42f391cc673d74cc2
Akhmedov76 <ahmedovj7686@gmail.com>
2025-02-16 23:39:56 +0500

    create admin register

--- app_chats/admin.py
+++ app_chats/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from app_chats.models import Chat
+
+
+class ChatAdmin(admin.ModelAdmin):
+    list_display = ('id', 'sender', 'receiver', 'message', 'timestamp', 'is_read')
+    list_filter = ('sender', 'receiver', 'timestamp', 'is_read')
+    search_fields = ('sender__email', 'receiver__email', 'message')
+    ordering = ('-id',)
+    fields = ('sender', 'receiver', 'message', 'timestamp', 'is_read')
+
+
+admin.site.register(Chat, ChatAdmin)
