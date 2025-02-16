2febc2239e28ddd6e206a40a036bd9739d302adf
Akhmedov76 <ahmedovj7686@gmail.com>
2025-02-16 15:13:30 +0500

    update Notification model

--- app_notifications/models.py
+++ app_notifications/models.py
@@ -9,3 +9,10 @@ class Notification(models.Model):
     message = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
     is_read = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Notification'
+        verbose_name_plural = 'Notifications'
