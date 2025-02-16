8686461aede6afeb9e743b81cd0fd17261d2201d
Akhmedov76 <ahmedovj7686@gmail.com>
2025-02-16 22:28:29 +0500

    update settings

--- conf/settings.py
+++ conf/settings.py
@@ -1,13 +1,15 @@
 import os
 from pathlib import Path
 
-BASE_DIR = Path(__file__).resolve().parent.parent
+from dotenv import load_dotenv
 
+BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv()
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'jazzmin',
