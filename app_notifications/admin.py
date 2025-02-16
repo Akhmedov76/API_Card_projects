3bb74070f4710ba8496b67e4f9d5debddf021e4a
Akhmedov76 <ahmedovj7686@gmail.com>
2025-02-16 23:39:33 +0500

    create admin register

--- app_notifications/admin.py
+++ app_notifications/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from app_notifications.models import Notification
+
+
+class NotificationAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'title', 'message', 'timestamp', 'is_read')
+    list_filter = ('user', 'timestamp', 'is_read')
+    search_fields = ('user__email', 'title', 'message')
+    ordering = ('-id',)
+    fields = ('user', 'title', 'message', 'timestamp', 'is_read')
+
+
+admin.site.register(Notification, NotificationAdmin)
