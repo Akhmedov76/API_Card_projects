aed557b35f760ce6dde87c71e3f38e9474fde9bc
Akhmedov76 <ahmedovj7686@gmail.com>
2025-02-16 15:09:43 +0500

    update test model

--- app_tests/models.py
+++ app_tests/models.py
@@ -6,3 +6,8 @@ class Test(models.Model):
     team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='tests')
     questions = models.TextField()
     answers = models.TextField()
+
+    class Meta:
+        verbose_name = 'Test'
+        verbose_name_plural = 'Tests'
+        ordering = ['-id']
