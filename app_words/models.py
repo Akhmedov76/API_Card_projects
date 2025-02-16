5f19b497630c4d92b16eb7a5d9fe1a0f2132fd6b
Akhmedov76 <ahmedovj7686@gmail.com>
2025-02-16 15:10:54 +0500

    update words model

--- app_words/models.py
+++ app_words/models.py
@@ -11,6 +11,13 @@ class WordCard(models.Model):
     team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='word_cards')
     created_at = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return self.word
+
+    class Meta:
+        verbose_name = 'Word Card'
+        verbose_name_plural = 'Word Cards'
+
 
 class PrivateCard(models.Model):
     word = models.CharField(max_length=100)
@@ -18,3 +25,10 @@ class PrivateCard(models.Model):
     description = models.TextField()
     status = models.BooleanField(default=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='private_cards')
+
+    def __str__(self):
+        return self.word
+
+    class Meta:
+        verbose_name = 'Private Card'
+        verbose_name_plural = 'Private Cards'
