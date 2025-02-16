f91976c8754e3f0a8b3382f9383d7ed0b13d46a5
Akhmedov76 <ahmedovj7686@gmail.com>
2025-02-16 15:11:29 +0500

    update team model

--- app_teams/models.py
+++ app_teams/models.py
@@ -7,3 +7,10 @@ class Team(models.Model):
     name = models.CharField(max_length=100)
     members = models.ManyToManyField(User, related_name='teams')
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Team'
+        verbose_name_plural = 'Teams'
