3822c58690d52cbd00242290995a0dec6a4d3e60
Akhmedov76 <ahmedovj7686@gmail.com>
2025-02-16 15:16:37 +0500

    update Chat model

--- app_chats/models.py
+++ app_chats/models.py
@@ -9,3 +9,10 @@ class Chat(models.Model):
     message = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
     is_read = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'{self.sender.get_full_name()} - {self.receiver.get_full_name()}'
+
+    class Meta:
+        verbose_name = 'Chat'
+        verbose_name_plural = 'Chats'
